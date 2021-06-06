5c139cc7f85a9ec0842e05654be829512d00fea2
BLACK-KILLER <63947251+ShuBhamg0sain@users.noreply.github.com>
2021-06-06 18:37:21 +0530

    Create  RemoteActionCompatParcelizer.java

---
+++ grow_follow/sources/android/support/p075v4/app/RemoteActionCompatParcelizer.java
@@ -0,0 +1,15 @@
+package android.support.p075v4.app;
+
+import androidx.core.app.RemoteActionCompat;
+import p000a.p074w.C0785a;
+
+/* renamed from: android.support.v4.app.RemoteActionCompatParcelizer */
+public final class RemoteActionCompatParcelizer extends androidx.core.app.RemoteActionCompatParcelizer {
+    public static RemoteActionCompat read(C0785a aVar) {
+        return androidx.core.app.RemoteActionCompatParcelizer.read(aVar);
+    }
+
+    public static void write(RemoteActionCompat remoteActionCompat, C0785a aVar) {
+        androidx.core.app.RemoteActionCompatParcelizer.write(remoteActionCompat, aVar);
+    }
+}
